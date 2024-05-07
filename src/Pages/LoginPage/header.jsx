f4d1f5652fca13f72773e59098d72ffb717ac105
DiegoDuaS <dua22075@uvg.edu.gt>
2024-05-06 20:25:27 -0600

    fix: changed to be more responsive

--- src/Pages/LoginPage/header.jsx
+++ src/Pages/LoginPage/header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './header.css'
+import useNavigate from '../../Hooks/useNavigation'
 
 function HeaderLogin () {
+  const { navigate } = useNavigate()
+
   return (
         <>
-        <header className='main'>
-            <div className = 'restitle'> Pixel Playgrounds Admin</div>
+        <header className='lmain'>
+            <div className = 'restitlel'> Pixel Playgrounds Admin</div>
+            <button className='gomain' onClick={() => navigate('/')}>Pagina Principal</button>
         </header>
         </>
   )
--- src/Pages/MainPage/header.jsx
+++ src/Pages/MainPage/header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './header.css'
+import useNavigate from '../../Hooks/useNavigation'
 
 function HeaderMain () {
+  const { navigate } = useNavigate()
+
   return (
         <>
         <header className='main'>
             <div className = 'restitle'> Pixel Playgrounds </div>
+            <button className ='goadmin' onClick={() => navigate('/admin')}>Administracion</button>
         </header>
         </>
   )
